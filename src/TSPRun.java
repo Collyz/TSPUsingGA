1378894389ed038d413cdf57defc3c977aadd417
BrendanLeeGit <brendanlee09090@gmail.com>
2023-04-30 22:52:32 -0400

    Changing run

--- src/TSPRun.java
+++ src/TSPRun.java
@@ -2,10 +2,24 @@ import java.io.IOException;
 
 public class TSPRun {
     public static void main(String[] args) throws IOException {
-        //String filename = args[0];
-        TSPReader reader = new TSPReader("dj38.tsp");
+        String filename;
+        int runsNumber;
+        int B;
+
+        if (args.length == 1){
+            filename = args[0];
+            runsNumber = 10000;
+            B = 4;
+        }
+        else{
+            filename = args[0];
+            runsNumber = Integer.parseInt(args[1]);
+            B = Integer.parseInt(args[2]);
+        }
+
+        TSPReader reader = new TSPReader(filename);
         reader.readFile();
-        VBSS vbss = new VBSS(reader.getNumOfCities(), 7, reader.getXCords(), reader.getYCords());
-        vbss.run(50000);
+        VBSS vbss = new VBSS(reader.getNumOfCities(), B, reader.getXCords(), reader.getYCords());
+        vbss.run(runsNumber);
     }
 }
